eaf707c4fe43cd9ee241073d270b2152281b8a12
Romain Jagueneau <romain.jagueneau@external.engie.com>
2023-06-26 17:44:51 +0200

    majback

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser'); // Ajoutez ceci
+const cors = require('cors'); // Ajoutez ceci
+
+app.use(cors()); // Ajoutez ceci
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
-
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
   if (username === 'test' && password === 'test') {
-	console.log('ok');
+    console.log('ok');
     res.status(200).json({ status: 'success', message: 'Login successful' });
   } else {
-	console.log('ko')
+    console.log('ko')
     res.status(401).json({ status: 'error', message: 'Invalid username/password' });
   }
 });
